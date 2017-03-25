b143912ee4029a61a707743ca7a2b0b9bfdcc85a
koogawa <koogawa.app@gmail.com>
2017-03-25 21:28:44 +0900

    Add files

---
+++ CookieManager/CookieManager.h
@@ -0,0 +1,19 @@
+//
+//  CookieManager.h
+//  CookieManager
+//
+//  Created by koogawa on 2017/03/25.
+//  Copyright © 2017 koogawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CookieManager.
+FOUNDATION_EXPORT double CookieManagerVersionNumber;
+
+//! Project version string for CookieManager.
+FOUNDATION_EXPORT const unsigned char CookieManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CookieManager/PublicHeader.h>
+
+
